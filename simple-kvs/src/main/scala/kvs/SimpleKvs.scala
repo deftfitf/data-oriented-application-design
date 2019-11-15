5ad84d7d53da5e188431d1d1d1bd4365c61f2244
deftfitf <nextframeworks@gmail.com>
2019-11-15 14:42:53 +0900

    error handling

--- simple-kvs/src/main/scala/kvs/SimpleKvs.scala
+++ simple-kvs/src/main/scala/kvs/SimpleKvs.scala
@@ -5,7 +5,9 @@ import java.util.Scanner
 
 import cats.effect.{ExitCode, IO, IOApp, Sync}
 import cats.implicits._
+
 import scala.collection.mutable
+import scala.util.{Failure, Success, Try}
 
 class SimpleKvs(raf: RandomAccessFile, keyIndex: mutable.Map[String, Long]) {
 
@@ -13,18 +15,27 @@ class SimpleKvs(raf: RandomAccessFile, keyIndex: mutable.Map[String, Long]) {
   private final val VALUE_HEADER_SIZE = Integer.BYTES
   private final val HEADER_SIZE = Integer.BYTES * 2
   private final val MIN_DATA_SIZE = HEADER_SIZE + 2
-  private final val tombstone = Integer.MIN_VALUE
+  private final val TOMBSTONE = Integer.MIN_VALUE
 
-  def set(key: String, value: String): Unit = {
-    raf.seek(raf.length())
-    raf.writeBytes(value)
-    raf.writeInt(value.length)
-    raf.writeBytes(key)
-    raf.writeInt(key.length)
+  def set(key: String, value: String): Try[Unit] = {
+    val befPos = raf.length()
+    try {
+      raf.seek(raf.length())
+      raf.writeBytes(value)
+      raf.writeInt(value.length)
+      raf.writeBytes(key)
+      raf.writeInt(key.length)
+    } catch {
+      case e: java.io.IOException =>
+        raf.setLength(befPos)
+        return Failure(e)
+    }
     keyIndex.update(key, raf.getFilePointer)
+    Success(())
   }
 
   @scala.annotation.tailrec
+  @throws[java.io.IOException]
   private def seek(key: Array[Byte], befPos: Long): Option[String] =
     if (befPos >= MIN_DATA_SIZE) {
       var pos = befPos
@@ -41,7 +52,7 @@ class SimpleKvs(raf: RandomAccessFile, keyIndex: mutable.Map[String, Long]) {
       pos -= VALUE_HEADER_SIZE
       raf.seek(pos)
       val valueLen = raf.readInt()
-      if (valueLen == tombstone) {
+      if (valueLen == TOMBSTONE) {
         return None
       }
       pos -= valueLen
@@ -56,19 +67,34 @@ class SimpleKvs(raf: RandomAccessFile, keyIndex: mutable.Map[String, Long]) {
       }
     } else None
 
-  def get(key: String): Option[String] =
-    keyIndex.get(key) match {
-      case Some(pos) => seek(key.getBytes(), pos)
-      case None      => seek(key.getBytes(), raf.length())
+  def get(key: String): Option[String] = {
+    val pos = keyIndex.getOrElse(key, raf.length())
+    try {
+      seek(key.getBytes, pos)
+    } catch {
+      case _: java.io.IOException => None
     }
+  }
 
-  def del(key: String): Unit =
-    get(key) foreach { _ =>
-      raf.seek(raf.length())
-      raf.writeInt(tombstone)
-      raf.writeBytes(key)
-      raf.writeInt(key.length)
-      keyIndex.update(key, raf.getFilePointer)
+  def del(key: String): Try[Unit] =
+    get(key) match {
+      case Some(_) =>
+        val befPos = raf.length()
+        try {
+          raf.seek(raf.length())
+          raf.writeInt(TOMBSTONE)
+          raf.writeBytes(key)
+          raf.writeInt(key.length)
+        } catch {
+          case e: java.io.IOException =>
+            raf.setLength(befPos)
+            return Failure(e)
+        }
+        keyIndex.update(key, raf.getFilePointer)
+        Success(())
+
+      case None =>
+        Success(())
     }
 
 }
