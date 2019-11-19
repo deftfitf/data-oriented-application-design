426751187c1b06c65a2d72d5b0653da71d66689d
deftfitf <nextframeworks@gmail.com>
2019-11-19 19:16:22 +0900

    remove cats dependency, add akka

--- build.sbt
+++ build.sbt
@@ -25,7 +25,6 @@ lazy val commonSettings = Seq(
   scalacOptions in console -= "-Ywarn-unused-import",
   scalacOptions in Test ++= Seq("-Yrangepos"),
   libraryDependencies ++= Seq(
-    "org.typelevel" %% "cats-effect" % "2.0.0",
     "org.specs2" %% "specs2-core" % "4.8.0" % Test,
     "org.specs2" %% "specs2-mock" % "4.8.0" % Test,
     scalaTest % Test
@@ -42,6 +41,10 @@ lazy val root = (project in file("."))
 lazy val simpleKvs = (project in file("simple-kvs"))
   .settings(
     commonSettings,
+    libraryDependencies ++= Seq(
+      "com.typesafe.akka" %% "akka-actor-typed" % "2.6.0",
+      "ch.qos.logback" % "logback-classic" % "1.1.3" % Runtime
+    ),
     name := "simple-kvs"
   )
 
--- simple-kvs/src/main/scala/kvs/SimpleKvs.scala
+++ simple-kvs/src/main/scala/kvs/SimpleKvs.scala
@@ -1,10 +1,6 @@
 package kvs
 
 import java.io.RandomAccessFile
-import java.util.Scanner
-
-import cats.effect.{ExitCode, IO, IOApp, Sync}
-import cats.implicits._
 
 import scala.collection.mutable
 import scala.util.{Failure, Success, Try}
@@ -99,9 +95,7 @@ class SimpleKvs(raf: RandomAccessFile, keyIndex: mutable.Map[String, Long]) {
 
 }
 
-object SimpleKvs extends IOApp {
-
-  private val databaseFileName = "data/simplekvs/database.txt"
+object SimpleKvs {
 
   def apply(databaseFileName: String): SimpleKvs = {
     val file = new java.io.File(databaseFileName)
@@ -112,35 +106,5 @@ object SimpleKvs extends IOApp {
     val raf = new java.io.RandomAccessFile(file, "rw")
     new SimpleKvs(raf, mutable.Map.empty)
   }
-
-  def simpleKvs[F[_]: Sync](databaseFileName: String): F[SimpleKvs] =
-    Sync[F].delay(apply(databaseFileName))
-
-  def scanner[F[_]: Sync]: F[Scanner] = Sync[F].delay(new Scanner(System.in))
-
-  def interpreter[F[_]: Sync](sc: Scanner, kvs: SimpleKvs): F[Unit] =
-    for {
-      lineR <- Sync[F].delay(sc.nextLine().trim.split("\\s+")).attempt
-      _ <- lineR match {
-        case Right(line) =>
-          line(0) match {
-            case "set" =>
-              Sync[F].delay(kvs.set(line(1), line(2))) >> interpreter(sc, kvs)
-            case "get" =>
-              Sync[F].delay(println(kvs.get(line(1)))) >> interpreter(sc, kvs)
-            case other =>
-              Sync[F].raiseError[Unit](
-                new Throwable(s"can not supported this command: $other"))
-          }
-        case Left(e) => Sync[F].raiseError[Unit](e)
-      }
-    } yield ()
-
-  override def run(args: List[String]): IO[ExitCode] =
-    for {
-      kvs <- simpleKvs[IO](databaseFileName)
-      sc <- scanner[IO]
-      _ <- interpreter[IO](sc, kvs)
-    } yield ExitCode.Success
 
 }
